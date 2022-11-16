7e4fd7474235278bac87b1b40b067a5139af875a
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-11-15 22:21:57 -0300

    generating objectid in domain seedwork

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -2,6 +2,17 @@ import { ValueObject } from '@/@seedwork/domain/value-objects';
 
 export class UniqueEntityId extends ValueObject<string> {
   constructor(readonly id?: string) {
-    super(id || Math.floor(Math.random() * 100 + 1).toString());
+    super(id || UniqueEntityId.generateId());
+  }
+
+  private static generateId() {
+    const ObjectId = (
+      m = Math,
+      d = Date,
+      h = 16,
+      s = (s: number) => m.floor(s).toString(h),
+    ) =>
+      s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
+    return ObjectId();
   }
 }
