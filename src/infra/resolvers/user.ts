6a4846bb5e73ab6a07738a927966cf33bc480979
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-12-24 01:14:48 -0300

    fixing missing field decorator

--- src/infra/resolvers/user.ts
+++ src/infra/resolvers/user.ts
@@ -1,12 +1,16 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
+  @Field()
   name: string;
 }
 
 @ObjectType()
 export class CreateUserOutput {
+  @Field()
   user: User;
+
+  @Field()
   accessToken: string;
 }
