eec531adbdf729bfbb5c7eb3eb0cc0354cb25aa5
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-02-19 17:00:27 -0300

    fix: adjusting start grpc and network name

--- product/src/main.ts
+++ product/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { grpcClientOptions } from './client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+  app.startAllMicroservices();
+  await app.listen(4001);
 }
 bootstrap();
